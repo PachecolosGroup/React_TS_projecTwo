3a1450d57f76b9a886b1b356b05ef9af58859072
Pachecolosgroup <cesarppublicidad03@gmail.com>
2025-01-27 23:07:01 -0500

    Final Changes and the app is online using vite online mode

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # React + TypeScript + Vite
 
-This was my first time typing Typescript in a project made for a Html template and using React, hooks, states to make him more interactive, this will state here in this moment until I got more knowledge and finish upgrading and chaning the general type on Header.tsx 
+This was my first time typing Typescript in a project made for a Html template and using React, hooks, states to make him more interactive, changes, will be done as the project or I get more knowledge in the long run.
 
-Date: 27.01.2025
+
+Still pending to do more changes with {
+  quantity : any /// on header 
+} 
+
+And others more in the whole project, I promise to comeback and fix them once
+I got more understanding on TypeScript and React. 
+
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,101 +1,113 @@
-export default function Header( {cart, removeFromCart, increaseQuantity, decreaseQuantity, emptyCart} ) {
-    /// El contenido es lo que se va a mostrar en pantalla
-    return(
-        <>
 
-        <header className="py-5 header">
-        <div className="container-xl">
-            <div className="row justify-content-center justify-content-md-between">
-                <div className="col-8 col-md-3">
-                    <a href="index.html">
-                        <img className="img-fluid" src="/img/logo.svg" alt="imagen logo" />
-                    </a>
-                </div>
-                <nav className="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
-                    <div 
-                        className="carrito"
-                    >
-                        <img className="img-fluid" src="/img/carrito.png" alt="imagen carrito" />
+import { Guitar } from "../types"; /// Importo los types
 
-                        <div id="carrito" className="bg-white p-3">
-                            
-                            {/*Terneario*/}
+/// defino directamente cada cosa que esta recibiendo de acuero a los cambios anteriores y modificaciones en los types.
 
-                            {isEmpty ? (
-                                <p className="text-center">El carrito esta vacio</p>
-                            ) : (
-                            <>
-                                <table className="w-100 table">
-                                    <thead>
-                                        <tr>
-                                            <th>Imagen</th>
-                                            <th>Nombre</th>
-                                            <th>Precio</th>
-                                            <th>Cantidad</th>
-                                            <th></th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
+interface HeaderProps {
+  cart: Guitar[];
+  removeFromCart: (id: number) => void;
+  increaseQuantity: (id: number) => void;
+  decreaseQuantity: (id: number) => void;
+  emptyCart: () => void;
+}
 
-                                        {cart.map((guitar) => ( 
-                                            <tr key={guitar.id}>
-                                            <td>
-                                                <img 
-                                                    className="img-fluid" 
-                                                    src={`/img/${guitar.image}.jpg`}
-                                                    alt="imagen guitarra" 
-                                                />
-                                            </td>
-                                            <td>{guitar.name}</td>
-                                            <td className="fw-bold">{guitar.price}</td>
-                                            <td className="flex align-items-start gap-4">
-                                                <button
-                                                    type="button"
-                                                    className="btn btn-dark"
-                                                    onClick={() => decreaseQuantity(guitar.id)} // Cambiado aquí
-                                                >
-                                                    -
-                                                </button>
-                                                {guitar.quantity}
-                                                <button
-                                                    type="button"
-                                                    className="btn btn-dark"
-                                                    onClick={() => increaseQuantity(guitar.id)}
-                                                >
-                                                    +
-                                                </button>
-                                            </td>
-                                            <td>
-                                                <button 
-                                                    className="btn btn-danger"
-                                                    type="button"
-                                                    onClick={() => removeFromCart(guitar.id)}
-                                                >
-                                                    X
-                                                </button>
-                                            </td>
-                                        </tr>
-                                            
-                                        ))}  
-                                    </tbody>
-                                </table>
-                            
-                            <p className="text-end"> Total pagar: <span
-                            className="fw-bold"> ${ cartTotal } </span></p>
-                            </>
-                            )}
-                            <button 
-                            className="btn btn-dark w-100 mt-3 p-2"
-                            onClick={ emptyCart }> 
-                            Vaciar Carrito </button>
+/// Exporto de una manera mas orderanada las funciones que utilizo, y la parte logica la separado de la interfaz directa del component
 
-                        </div>
-                    </div>
-                </nav>
+export default function Header({
+  cart,
+  removeFromCart,
+  increaseQuantity,
+  decreaseQuantity,
+  emptyCart,
+}: HeaderProps): JSX.Element {
+  const isEmpty = cart.length === 0; /// con esto se comprueba si el carrito esta vacio
+  const cartTotal = cart.reduce((total, item) => total + item.price * item.quantity, 0); 
+// Matengo el error de quantity, regresare cuando tenga mas idea de como mejorarlo, a pesar de que hice la declaracion de quantity
+
+  return (
+    <header className="py-5 header">
+      <div className="container-xl">
+        <div className="row justify-content-center justify-content-md-between">
+          <div className="col-8 col-md-3">
+            <a href="index.html">
+              <img className="img-fluid" src="/img/logo.svg" alt="imagen logo" />
+            </a>
+          </div>
+          <nav className="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
+            <div className="carrito">
+              <img className="img-fluid" src="/img/carrito.png" alt="imagen carrito" />
+
+              <div id="carrito" className="bg-white p-3">
+                {isEmpty ? (
+                  <p className="text-center">El carrito está vacío</p>
+                ) : (
+                  <>
+                    <table className="w-100 table">
+                      <thead>
+                        <tr>
+                          <th>Imagen</th>
+                          <th>Nombre</th>
+                          <th>Precio</th>
+                          <th>Cantidad</th>
+                          <th></th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {cart.map((item) => (
+                          <tr key={item.id}>
+                            <td>
+                              <img
+                                className="img-fluid"
+                                src={`/img/${item.image}.jpg`}
+                                alt={`Imagen de ${ item.name}`}
+                              />
+                            </td>
+                            <td>{item.name}</td>
+                            <td className="fw-bold">${item.price}</td>
+                            <td className="flex align-items-start gap-4">
+                              <button
+                                type="button"
+                                className="btn btn-dark"
+                                onClick={() => decreaseQuantity(item.id)}
+                              >
+                                -
+                              </button>
+                              {item.quantity}
+                              <button
+                                type="button"
+                                className="btn btn-dark"
+                                onClick={() => increaseQuantity(item.id)}
+                              >
+                                +
+                              </button>
+                            </td>
+                            <td>
+                              <button
+                                className="btn btn-danger"
+                                type="button"
+                                onClick={() => removeFromCart(item.id)}
+                              >
+                                X
+                              </button>
+                            </td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
+
+                    <p className="text-end">
+                      Total a pagar: <span className="fw-bold">${cartTotal}</span>
+                    </p>
+                  </>
+                )}
+                <button className="btn btn-dark w-100 mt-3 p-2" onClick={emptyCart}>
+                  Vaciar Carrito
+                </button>
+              </div>
             </div>
+          </nav>
         </div>
+      </div>
     </header>
-    </>
-
-    )
+  );
 }
